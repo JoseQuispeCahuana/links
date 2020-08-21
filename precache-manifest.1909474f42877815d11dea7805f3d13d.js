aa7a020f849a9aa78473d5a04a18a66ba94c23db
JoseQuispeCahuana <lacarpetadelvergazo1@gmail.com>
2020-08-21 08:30:59 -0500

    Updates

---
+++ precache-manifest.1909474f42877815d11dea7805f3d13d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f44802f35fb75c010cf66d6d31a2f37",
+    "url": "/links/index.html"
+  },
+  {
+    "revision": "94093f8a988f3af9bef7",
+    "url": "/links/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "710626ff6706b65cf69c",
+    "url": "/links/static/js/2.7e58e27d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/links/static/js/2.7e58e27d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94093f8a988f3af9bef7",
+    "url": "/links/static/js/main.eab01e17.chunk.js"
+  },
+  {
+    "revision": "f8ebb95424c22992441e",
+    "url": "/links/static/js/runtime-main.ee5cba92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/links/static/media/logo.5d5d9eef.svg"
+  }
+]);
